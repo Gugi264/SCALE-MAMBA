077731c680e72e4f603f5568781a637eb91d5a38
Simon Guggi <simon.guggi@iaik.tugraz.at>
2022-09-15 19:30:54 +0200

    wip

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./compile.sh Programs/mult_demo
+./compile.sh Programs/mult
 for i in 1 2 3 4 5
 do
-	./Player.x $i -t Programs/mult_demo > out${i}.txt 2>&1 &
+	./Player.x $i -t Programs/mult > out${i}.txt 2>&1 &
 done
 
-time ./Player.x 0 -t -v 1 Programs/mult_demo | tee out0.txt 2>&1 
+time ./Player.x 0 -t -v 1 Programs/mult | tee out0.txt 2>&1 
 
