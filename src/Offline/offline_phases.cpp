4d32eda59dc5b34eeb374356e1550656b865d375
Simon Guggi <simon.guggi@iaik.tugraz.at>
2023-03-31 10:13:55 +0200

    changed batch size again

--- src/Offline/offline_phases.cpp
+++ src/Offline/offline_phases.cpp
@@ -138,14 +138,14 @@ void mult_phase(int num_online, Player &P, int fake_sacrifice,
   list<Share>::iterator it;
   int flag;
   int max_batch_size = 30000;
-  int actual_batch;
-  if (OCD.minm > max_batch_size) {
-        actual_batch = max_batch_size;
-  } else if (OCD.minm == 0) {
-        actual_batch = 5000;
-  } else {
-        actual_batch = OCD.minm;
-  }
+  int actual_batch = 10000;
+//  if (OCD.minm > max_batch_size) {
+//        actual_batch = max_batch_size;
+//  } else if (OCD.minm == 0) {
+//        actual_batch = 5000;
+//  } else {
+//        actual_batch = OCD.minm;
+//  }
 
   cout << "OCD.minm" << OCD.minm << endl;
   cout << "actual_batch: " << actual_batch << endl;
